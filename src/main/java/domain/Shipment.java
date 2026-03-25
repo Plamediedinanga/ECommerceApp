64852080f55ea6fbe06ebaa93dbeef3c2849d079
Tlangelani Chauke <222520663@mycput.ac.za>
2026-03-25 19:46:15 +0200

    Add files via upload

---
+++ src/main/java/domain/Shipment.java
@@ -0,0 +1,90 @@
+
+package za.ac.cput.domain;
+
+import java.util.Date;
+
+public class Shipment {
+
+    private String shipmentId;
+    private String address;
+    private Date shipmentDate;
+    private Date deliveryDate;
+    private String status;
+
+    // Private constructor
+    private Shipment(Builder builder) {
+        this.shipmentId = builder.shipmentId;
+        this.address = builder.address;
+        this.shipmentDate = builder.shipmentDate;
+        this.deliveryDate = builder.deliveryDate;
+        this.status = builder.status;
+    }
+
+    // Getters
+    public String getShipmentId() {
+        return shipmentId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Date getShipmentDate() {
+        return shipmentDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    // Builder Class
+    public static class Builder {
+        private String shipmentId;
+        private String address;
+        private Date shipmentDate;
+        private Date deliveryDate;
+        private String status;
+
+        public Builder setShipmentId(String shipmentId) {
+            this.shipmentId = shipmentId;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setShipmentDate(Date shipmentDate) {
+            this.shipmentDate = shipmentDate;
+            return this;
+        }
+
+        public Builder setDeliveryDate(Date deliveryDate) {
+            this.deliveryDate = deliveryDate;
+            return this;
+        }
+
+        public Builder setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder copy(Shipment shipment) {
+            this.shipmentId = shipment.shipmentId;
+            this.address = shipment.address;
+            this.shipmentDate = shipment.shipmentDate;
+            this.deliveryDate = shipment.deliveryDate;
+            this.status = shipment.status;
+            return this;
+        }
+
+        public Shipment build() {
+            return new Shipment(this);
+        }
+    }
+}

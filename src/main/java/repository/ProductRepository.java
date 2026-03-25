99eee84b34e6e9850d37639313e0619d74b1d5e6
Plamedie Lubuya Dinanga <165044495+Plamediedinanga@users.noreply.github.com>
2026-03-25 11:58:43 +0200

    Refactor ProductRepository to extend IRepository
    Updated the ProductRepository interface to extend IRepository and added a method to find products by name.

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,18 +1,14 @@
 /*
  * ProductRepository.java
- * Repository Interface for Product
- * Author: Plamedie Dinanga 230082629
- * Date: 21 March 2026
+ * Product Repository Interface
+ * Author: Plamedie 230082629
+ * Date: 24 March 2026
  */
 package repository;
 
 import domain.Product;
-import java.util.List;
 
-public interface ProductRepository {
-    Product save(Product product);
-    Product findById(String productId);
-    List<Product> findAll();
-    void delete(String productId);
-    Product update(Product product);
+public interface ProductRepository extends IRepository<Product, String> {
+    // Additional product-specific methods if needed
+    Product findByProductName(String productName);
 }

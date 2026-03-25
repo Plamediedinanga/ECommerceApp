63979d5b1637a126a16eb85e1d46bddcc2deb833
Tlangelani Chauke <222520663@mycput.ac.za>
2026-03-25 19:46:42 +0200

    Factory

---
+++ src/main/java/factory/ShipmentFactory.java
@@ -0,0 +1,37 @@
+
+package za.ac.cput.factory;
+
+import za.ac.cput.domain.Shipment;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class ShipmentFactory {
+
+    public static Shipment createShipment(String address,
+                                          Date shipmentDate,
+                                          Date deliveryDate,
+                                          String status) {
+
+        // Validation
+        if (address == null || address.isEmpty())
+            return null;
+
+        if (shipmentDate == null || deliveryDate == null)
+            return null;
+
+        if (status == null || status.isEmpty())
+            return null;
+
+        // Generate ID
+        String shipmentId = UUID.randomUUID().toString();
+
+        return new Shipment.Builder()
+                .setShipmentId(shipmentId)
+                .setAddress(address)
+                .setShipmentDate(shipmentDate)
+                .setDeliveryDate(deliveryDate)
+                .setStatus(status)
+                .build();
+    }
+}
